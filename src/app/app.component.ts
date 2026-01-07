b41cc1301c127f4cf2f72b1b1230facb7be37153
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2026-01-07 03:28:47 +0000

    Conclusão da lógica. Resolução de bugs e começo do aprefeiçoamenteo da UI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Board } from './game/board'
+import { NgFor, NgIf, NgClass } from '@angular/common';
+import { Cell } from './game/cell';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  // Debug Variables
+  numbers = [1,2,3,4,5,6,7,8];
+  specialChars = ['`', '*', "\u{1F60E}", "\u{1F62E}", "\u{1F635}", "\u{1F642}"];
+
   title = 'Minesweeper';
+  board = new Board(5, 5);
+
+  // TODO Difficulties:
+  // 10 on Beginner (9x9 grid), 40 on Intermediate (16x16 grid), and 99 on Expert (30x16 grid) & Custom (any dimension, any bombs)
+
+  checkCell(cell: Cell) {
+    if (this.board.result === null) {
+      this.board.result = this.board.checkCell(cell);
+      // console.log(this.result)
+      
+      if (this.board.result === 'gameover') {
+        alert('You lose');
+      } else if (this.board.result === 'win') {
+        alert('You win');
+      }
+    }
+  }
+
+  flag(cell: Cell) {
+    if (this.board.result === null) {
+      if(cell.status === 'flag') {
+        cell.status = 'open';
+      } else if (cell.status !== 'clear') {
+        cell.status = 'flag';
+      }
+    }
+  }
+
+  reset() {
+    this.board = new Board(5, 5);
+  }
 }
---
+++ src/app/game/board.ts
@@ -0,0 +1,106 @@
+import { Cell } from './cell'
+
+const PROXIMITY = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
+export class Board {
+    cells: Cell[][] = []
+    remainingCells = 0;
+    mineCount = 0;
+    result: string | null = null;
+
+    constructor(size: number, mines: number) {
+        // Rows
+        for (let row = 0; row < size; row++) {
+            this.cells[row] = [];
+
+            // Cols
+            for (let col = 0; col < size; col++) {
+                this.cells[row][col] = new Cell(row, col);
+            }
+        }
+
+        // Assign Mines
+        for (let i = 0; i < mines; i++) {
+            this.getRandomCell().mine = true;
+        }
+
+        // Count mines
+        for (let row = 0; row < size; row++) {
+            for (let col = 0; col < size; col++) {
+                let adjacentMines = 0;
+
+                for (let prox of PROXIMITY) {
+                    if (
+                        this.cells[row + prox[0]] &&
+                        this.cells[row + prox[0]][col + prox[1]] &&
+                        this.cells[row + prox[0]][col + prox[1]].mine
+                    ) {
+                        adjacentMines++
+                    }
+                }
+                this.cells[row][col].proximityMines = adjacentMines;
+
+                if (this.cells[row][col].mine) {
+                    this.mineCount++;
+                }
+            }
+        }
+        this.remainingCells = (size * size) - this.mineCount;
+    }
+
+    getRandomCell(): Cell {
+        const row = Math.floor(Math.random() * this.cells.length);
+        const col = Math.floor(Math.random() * this.cells[row].length);
+        return this.cells[row][col]
+    }
+
+    checkCell(cell: Cell): string | null {
+        // console.log(cell);
+        let condition = '';
+
+        if (cell.status !== "open") {
+            //condition = null;
+            return null;
+        } else if (cell.mine) {
+            this.revealAll();
+            condition = 'gameover';
+        } else {
+            cell.status = "clear";
+
+            if(cell.proximityMines === 0) {
+                for (let prox of PROXIMITY) {
+                    if (
+                        this.cells[cell.row + prox[0]] &&
+                        this.cells[cell.row + prox[0]][cell.column + prox[1]]
+                    ) {
+                        this.checkCell(this.cells[cell.row + prox[0]][cell.column + prox[1]])
+                    }
+                }
+            }
+
+            if(this.remainingCells-- <= 1) {
+                condition = 'win';
+            }
+        }
+
+        if (condition !== '') {
+            this.revealAll();
+            return condition;
+        }
+
+        return null;
+    }
+
+    revealAll() {
+        for (const row of this.cells) {
+            for (const cell of row) {
+                // if (cell.status === 'open') {
+                //     cell.status = 'clear';
+                // }
+                if (cell.mine) {
+                    cell.status = 'clear';
+                }
+            }
+        }
+    }
+}
